80f42c26db3b56a6b1927e45cf5f6164f29d726c
miguelangel <miguelangelmedina2494@gmail.com>
2021-08-15 10:43:15 -0400

    Add Navbar v1

--- JokeWebApp/Data/Migrations/20210815135813_initial setup.Designer.cs
+++ JokeWebApp/Data/Migrations/20210815135813_initial setup.Designer.cs
@@ -4,14 +4,16 @@ using JokeWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokeWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210815135813_initial setup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
