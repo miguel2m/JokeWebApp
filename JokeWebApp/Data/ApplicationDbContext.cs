3dc949be816cf0e8b896344c804fc22b32fec213
miguelangel <miguelangelmedina2494@gmail.com>
2021-08-15 09:56:49 -0400

    Add Controller Joke

--- JokeWebApp/Data/ApplicationDbContext.cs
+++ JokeWebApp/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using JokeWebApp.Models;
 
 namespace JokeWebApp.Data
 {
@@ -12,5 +13,6 @@ namespace JokeWebApp.Data
             : base(options)
         {
         }
+        public DbSet<JokeWebApp.Models.Joke> Joke { get; set; }
     }
 }
